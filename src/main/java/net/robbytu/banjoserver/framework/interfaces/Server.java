253b117433c95cff098d3c85c0d9a225c9ee92f4
Robert de Vries <r.devries@robbytu.net>
2013-05-12 20:19:35 +0200

    Created a new Server class for later use

---
+++ src/main/java/net/robbytu/banjoserver/framework/interfaces/Server.java
@@ -0,0 +1,12 @@
+package net.robbytu.banjoserver.framework.interfaces;
+
+import org.bukkit.entity.Player;
+
+public class Server {
+	
+	public String serverName;
+	public int serverStatus;
+	public int serverPlayers;
+	public Player[] players;
+	
+}
