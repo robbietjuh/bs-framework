877b401198320832dd0d4792917de0855fb15e67
Robert de Vries <r.devries@robbytu.net>
2013-05-12 20:31:10 +0200

    Implemented ServerAPI

--- src/main/java/net/robbytu/banjoserver/framework/api/ServerAPI.java
+++ src/main/java/net/robbytu/banjoserver/framework/api/ServerAPI.java
@@ -1,5 +1,11 @@
 package net.robbytu.banjoserver.framework.api;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import net.robbytu.banjoserver.framework.Main;
 import net.robbytu.banjoserver.framework.interfaces.Server;
 
 public class ServerAPI {
@@ -9,8 +15,35 @@ public class ServerAPI {
 	 * @return An array of servers
 	 */
 	public static Server[] getServers() {
-		// Todo
-		return null;
+		// Init some vars
+		Connection conn = Main.conn;
+		Server[] servers = {};
+		
+		try {
+			// Create a new select statement
+			Statement select = conn.createStatement();
+			ResultSet result = select.executeQuery("SELECT servername, online, players FROM bs_servers");
+			
+			// For each server ...
+			while(result.next()) {
+				// Create a new Server instance
+				Server server = new Server();
+				
+				// Fill in properties
+				server.serverName = result.getString(0);
+				server.serverStatus = result.getInt(1);
+				server.serverPlayers = result.getInt(2);
+				
+				// Add server to return array
+				servers[servers.length] = server;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// Return the array of servers
+		return servers;
 	}
 	
 }
