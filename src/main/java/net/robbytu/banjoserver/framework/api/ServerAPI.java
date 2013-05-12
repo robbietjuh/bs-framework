bb1f93509643ad864569f33b3382ddcc287c71e8
Robert de Vries <r.devries@robbytu.net>
2013-05-12 20:19:50 +0200

    ServerAPI class (still needs implementation, though)

---
+++ src/main/java/net/robbytu/banjoserver/framework/api/ServerAPI.java
@@ -0,0 +1,16 @@
+package net.robbytu.banjoserver.framework.api;
+
+import net.robbytu.banjoserver.framework.interfaces.Server;
+
+public class ServerAPI {
+	
+	/**
+	 * Fetch all servers, including their status and online players
+	 * @return An array of servers
+	 */
+	public static Server[] getServers() {
+		// Todo
+		return null;
+	}
+	
+}
