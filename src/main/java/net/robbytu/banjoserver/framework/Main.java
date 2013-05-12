1977dff8c074b3585ac6f5c18d7f14910e74343c
Robert de Vries <r.devries@robbytu.net>
2013-05-12 20:02:14 +0200

    Basic configuration implementation

--- src/main/java/net/robbytu/banjoserver/framework/Main.java
+++ src/main/java/net/robbytu/banjoserver/framework/Main.java
@@ -5,6 +5,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
+		// Fetch db configuration
+		String host = getConfig().getString("db.host");
+		String user = getConfig().getString("db.username");
+		String pass = getConfig().getString("db.password");
+		String database = getConfig().getString("db.database");
+		
+		if(host.isEmpty() || user.isEmpty() || pass.isEmpty() || database.isEmpty()) {
+			getLogger().warning("Framework has not been enabled. Please check your configuration.");
+			return;
+		}
+		
+		
+		
 		getLogger().info("Framework has been enabled.");
 	}
 	
