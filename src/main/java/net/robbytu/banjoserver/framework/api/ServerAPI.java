0f89599edbd6116997704f4889510905dad9061d
Robert de Vries <r.devries@robbytu.net>
2013-05-12 21:22:02 +0200

    Implemented methods for fetching online, offline and all servers in ServerAPI

--- src/main/java/net/robbytu/banjoserver/framework/api/ServerAPI.java
+++ src/main/java/net/robbytu/banjoserver/framework/api/ServerAPI.java
@@ -11,10 +11,10 @@ import net.robbytu.banjoserver.framework.interfaces.Server;
 public class ServerAPI {
 	
 	/**
-	 * Fetch all servers, including their status and online players
+	 * Fetch all servers and filter them with a statement, including their status and online player count
 	 * @return An array of servers
 	 */
-	public static Server[] getServers() {
+	public static Server[] getServers(String statement) {
 		// Init some vars
 		Connection conn = Main.conn;
 		Server[] servers = {};
@@ -22,7 +22,7 @@ public class ServerAPI {
 		try {
 			// Create a new select statement
 			Statement select = conn.createStatement();
-			ResultSet result = select.executeQuery("SELECT servername, online, players FROM bs_servers");
+			ResultSet result = select.executeQuery("SELECT servername, online, players FROM bs_servers" + statement);
 			
 			// For each server ...
 			while(result.next()) {
@@ -46,4 +46,28 @@ public class ServerAPI {
 		return servers;
 	}
 	
+
+	/**
+	 * Fetch all servers, including their status and online player count
+	 * @return An array of servers
+	 */
+	public static Server[] getServers() {
+		return getServers("");
+	}
+	
+	/**
+	 * Fetch all online servers, including their status and online player count
+	 * @return An array of servers
+	 */
+	public static Server[] getOnlineServers() {
+		return getServers(" WHERE online = 1");
+	}
+
+	/**
+	 * Fetch all offline servers, including their status and online player count
+	 * @return An array of servers
+	 */
+	public static Server[] getOfflineServers() {
+		return getServers(" WHERE online = 0");
+	}
 }
