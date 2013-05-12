88fe3c9af8941de100c9fc62ccd507e87178a7af
Robert de Vries <r.devries@robbytu.net>
2013-05-12 20:49:01 +0200

    Implemented online player count updater

--- src/main/java/net/robbytu/banjoserver/framework/events/ServerUpdater.java
+++ src/main/java/net/robbytu/banjoserver/framework/events/ServerUpdater.java
@@ -9,10 +9,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ServerUpdater implements Listener {
 	private Connection conn = Main.conn;
 	
+	/**
+	 * Updates server online status
+	 * @param online Whether the server is online or not
+	 */
 	public void setOnline(int online) {
 		// Update our Server entry in the database
 		try {
@@ -24,8 +29,28 @@ public class ServerUpdater implements Listener {
 		}
 	}
 	
+	/**
+	 * Updates online player count
+	 */
+	private void updatePlayerCount() {
+		// Update online count
+		try {
+			int onlineCount = Bukkit.getServer().getOnlinePlayers().length;
+			this.conn.createStatement().executeQuery("UPDATE bs_servers SET players = " + onlineCount + " WHERE servername = '" + Bukkit.getServer().getServerName() + "'");
+		}
+		catch (SQLException e) {
+			Bukkit.getLogger().warning("Framework could not update online player count.");
+			e.printStackTrace();
+		}
+	}
+	
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent event) {
-        // Todo
+        this.updatePlayerCount();
+    }
+    
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        this.updatePlayerCount();
     }
 }
