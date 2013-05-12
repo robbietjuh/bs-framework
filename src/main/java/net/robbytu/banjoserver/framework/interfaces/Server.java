ac101c610217787fa98864e922ecf7ed653dc1a2
Robert de Vries <r.devries@robbytu.net>
2013-05-12 20:22:49 +0200

    Updated Server class, documentation

--- src/main/java/net/robbytu/banjoserver/framework/interfaces/Server.java
+++ src/main/java/net/robbytu/banjoserver/framework/interfaces/Server.java
@@ -1,12 +1,28 @@
 package net.robbytu.banjoserver.framework.interfaces;
 
-import org.bukkit.entity.Player;
-
+/**
+ * Server class
+ * 
+ * @author Robert
+ *
+ */
 public class Server {
 	
+	/**
+	 * Name of the server
+	 */
 	public String serverName;
+	
+	/**
+	 * Status of this server:
+	 *   0 = offline
+	 *   1 = online
+	 */
 	public int serverStatus;
+	
+	/**
+	 * Total amount of players online
+	 */
 	public int serverPlayers;
-	public Player[] players;
 	
 }
