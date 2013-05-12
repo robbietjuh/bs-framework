f09140ab24a3f9fc80d5b4c55ad095d207d2dc5a
Robert de Vries <r.devries@robbytu.net>
2013-05-12 20:10:47 +0200

    onEnable will now try to set up a MySQL connection

--- src/main/java/net/robbytu/banjoserver/framework/Main.java
+++ src/main/java/net/robbytu/banjoserver/framework/Main.java
@@ -2,22 +2,37 @@ package net.robbytu.banjoserver.framework;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main extends JavaPlugin {
+	public Connection conn = null;
+	
 	@Override
 	public void onEnable() {
 		// Fetch db configuration
 		String host = getConfig().getString("db.host");
+		int port = getConfig().getInt("db.port");
 		String user = getConfig().getString("db.username");
 		String pass = getConfig().getString("db.password");
 		String database = getConfig().getString("db.database");
 		
+		// Check db configuration
 		if(host.isEmpty() || user.isEmpty() || pass.isEmpty() || database.isEmpty()) {
 			getLogger().warning("Framework has not been enabled. Please check your configuration.");
 			return;
 		}
 		
+		// Set up a connection
+		try{
+			conn = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, user, pass);
+		}
+		catch (Exception e) {
+			getLogger().warning("Framework has not been enabled. Please check your configuration.");
+			getLogger().warning(e.toString());
+		}
 		
-		
+		// Everything went OK
 		getLogger().info("Framework has been enabled.");
 	}
 	
