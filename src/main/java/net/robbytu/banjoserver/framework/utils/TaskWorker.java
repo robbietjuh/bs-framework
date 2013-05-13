88b9698e50e8e56751235294e88560c430c1ce16
Robert de Vries <r.devries@robbytu.net>
2013-05-13 17:36:56 +0200

    Added TaskWorker class

---
+++ src/main/java/net/robbytu/banjoserver/framework/utils/TaskWorker.java
@@ -0,0 +1,18 @@
+package net.robbytu.banjoserver.framework.utils;
+
+import net.robbytu.banjoserver.framework.Main;
+import org.bukkit.Bukkit;
+
+import java.sql.Connection;
+
+public class TaskWorker {
+
+    private Connection conn = Main.conn;
+
+    /**
+     * Initializes a new TaskWorker
+     */
+    public TaskWorker() {
+    }
+
+}
