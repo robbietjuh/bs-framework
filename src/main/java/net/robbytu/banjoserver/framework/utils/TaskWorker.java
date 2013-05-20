0971ef989ee4d0404ce2d30ff18bdc9a8ef4490d
Robert de Vries <r.devries@robbytu.net>
2013-05-20 17:32:54 +0200

    TaskWorker now marks tasks as completed once they've been executed

--- src/main/java/net/robbytu/banjoserver/framework/utils/TaskWorker.java
+++ src/main/java/net/robbytu/banjoserver/framework/utils/TaskWorker.java
@@ -2,12 +2,12 @@ package net.robbytu.banjoserver.framework.utils;
 
 import net.robbytu.banjoserver.framework.Main;
 import org.bukkit.Bukkit;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class TaskWorker {
     /**
@@ -36,6 +36,8 @@ public class TaskWorker {
             Statement select = conn.createStatement();
             ResultSet result = select.executeQuery("SELECT id, tasktype, task, as_user FROM bs_tasks WHERE server = '" + Bukkit.getServer().getServerName() + "' AND completed = 0");
 
+            ArrayList<String> completedTasks = new ArrayList<String>();
+
             // For each task ...
             while(result.next()) {
                 switch(result.getInt(2)) {
@@ -50,7 +52,18 @@ public class TaskWorker {
                         Bukkit.getLogger().warning("Got a task but didn't know what to do with it. Type seems to be " + result.getInt(2));
                         break;
                 }
+
+                // Add ID to the complete-array
+                completedTasks.add("" + result.getInt(1));
             }
+
+            // Update all completed items
+            String updateStatement = "UPDATE bs_tasks SET completed = 1 WHERE ";
+            for(String taskId : completedTasks.toArray(new String[completedTasks.size()]))
+                updateStatement += "id = " + taskId + " OR ";
+
+            // Execute update query
+            conn.createStatement().executeUpdate(updateStatement.substring(-4));
         }
         catch (Exception ex) {
             Bukkit.getLogger().warning("Could not check for tasks due to an unexpected error.");
