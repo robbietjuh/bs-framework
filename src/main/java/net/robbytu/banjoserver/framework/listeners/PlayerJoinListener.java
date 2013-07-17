1eb94a23f7640ffa92d505958c468e919b696c5a
Robert de Vries <r.devries@robbytu.net>
2013-07-17 21:55:23 +0200

    Resolved BS-63 - Populate inventory upon first join

---
+++ src/main/java/net/robbytu/banjoserver/framework/listeners/PlayerJoinListener.java
@@ -0,0 +1,46 @@
+package net.robbytu.banjoserver.framework.listeners;
+
+import net.robbytu.banjoserver.framework.Main;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.inventory.ItemStack;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class PlayerJoinListener implements Listener {
+
+    @EventHandler
+    public void playerJoin(PlayerJoinEvent event) {
+        try {
+            Connection conn = Main.conn;
+
+            PreparedStatement statement = conn.prepareStatement("SELECT * FROM bs_firstjoin WHERE username = ?");
+            statement.setString(1, event.getPlayer().getName());
+            ResultSet result = statement.executeQuery();
+
+            boolean firstJoin = true;
+            while(result.next()) firstJoin = false;
+
+            if(firstJoin) {
+                statement = conn.prepareStatement("SELECT material, extra, amount FROM bs_defaultinvs WHERE server = ?");
+                statement.setString(1, Main.plugin.getServer().getServerName());
+                result = statement.executeQuery();
+
+                while(result.next()) {
+                    Material material = Material.getMaterial(result.getInt(1));
+                    ItemStack stack = new ItemStack(material, result.getInt(3));
+                    event.getPlayer().getInventory().addItem(stack);
+                }
+
+                event.getPlayer().sendMessage(ChatColor.YELLOW + "Welkom in de " + Main.plugin.getServer().getServerName() + " server van de Banjoserver, " + event.getPlayer().getName() + "!");
+            }
+        }
+        catch(Exception ignored) {}
+    }
+
+}
